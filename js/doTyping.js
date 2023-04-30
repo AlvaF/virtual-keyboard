95ec990b26cf126c0d308d515f47bd75571e6609
Alvina <Es_96@mail.ru>
2023-04-30 19:29:00 +0700

    feat: add function for typing on virtual keyboard

---
+++ js/doTyping.js
@@ -0,0 +1,23 @@
+export default function doTyping() {
+    const keyboard = document.querySelector('.keyboard');
+    let keyElements = document.querySelectorAll('.keyItem');
+    const textArea = document.querySelector('.text');
+    keyElements.forEach((el) => {
+        el.addEventListener('click', (ev) => {
+            console.log(ev.target.id);
+            console.log()
+            if (ev.target.id.length <= 1) {
+                textArea.value += ev.target.id
+            } else {
+                switch (ev.target.id) {
+                    case 'Backspace':
+                        textArea.value = textArea.value.substring(0, textArea.value.length - 1)
+                        break;
+                    case 'Space':
+                        textArea.value += " ";
+                        break;
+                }
+            }
+        })
+    })
+}
