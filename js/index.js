ee3963cc050ebdd58d1b1da204870a2f52fddff1
Alvina <Es_96@mail.ru>
2023-05-01 18:30:02 +0700

    fix: change buttons for changing language + change text in html

--- js/index.js
+++ js/index.js
@@ -7,7 +7,7 @@ const body = document.querySelector('body');
 
 const { engKeyboard, engCapsKeyboard, rusKeyboard, rusCapsKeyboard } = keyboards;
 body.innerHTML = "<textarea class='text'></textarea>"
-let out = '<div class="keyboard"></div><div class="describtion"><p>Зажмите Enter и Alt для смены языка</p></div>';
+let out = '<div class="keyboard"></div><div class="describtion"><p>Зажмите Shift и Alt для смены языка</p></div>';
 body.insertAdjacentHTML('afterbegin', out);
 
 
@@ -22,8 +22,8 @@ keyboardHighlights();
 
 
 function changeLang(event) {
-    // если зажаты клавиши Enter и Alt
-    if (event.code === 'Enter' && event.altKey) {
+    // если зажаты клавиши Shift и Alt
+    if (event.altKey && event.shiftKey) {
         if (curLang === 'en') {
             curLang = 'ru';
             capsPressed === true ? curKeyboard = rusCapsKeyboard : curKeyboard = rusKeyboard;
@@ -58,12 +58,6 @@ const pressCapsLock = (event) => {
 document.addEventListener('keydown', changeLang);
 document.addEventListener('keydown', pressCapsLock);
 document.querySelector('#CapsLock').addEventListener('click', pressCapsLock)
-// document.onload = function () {
-//     if (capsLockBtn.onclick) {
-//         capsLockBtn.removeEventListener('click', pressCapsLock);
-//     }
-//     capsLockBtn.addEventListener('click', pressCapsLock);
 
-// };
 
 
